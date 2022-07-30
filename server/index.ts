4496aa9820d9b3495fe1bd00bd1cafeeb8e3b91f
Ahmed Mohamed <jackiejeznach@gmail.com>
2022-07-31 03:25:39 +0400

    Idk what i did (AGAIN) 🗿📌

--- server/index.ts
+++ server/index.ts
@@ -34,11 +34,9 @@ app.get("/", (req, res) => {
 });
 app.get("/about", (req, res) => {
     res.sendFile(path.join(__dirname, '/public/other-pages/about/about.html'));
-    console.log(`Successfully Sent ${req.originalUrl} to ROOT app`)
 });
 app.get("/contact", (req, res) => { 
     res.sendFile(path.join(__dirname, '/public/other-pages/contact/contact.html'));
-    console.log(`Successfully Sent ${req.originalUrl} to ROOT app`)
 });
 
 
@@ -49,85 +47,71 @@ app.get("/contact", (req, res) => {
 
 app.get("/post-1", (req, res) => {
     res.sendFile(path.join(__dirname, '/public/posts/post_1.html'));
-    console.log(`Successfully Sent ${req.originalUrl} to ROOT app`)
 });
 
 app.get("/post-2", (req, res) => {
     res.sendFile(path.join(__dirname, '/public/posts/post_2.html'));
-    console.log(`Successfully Sent ${req.originalUrl} to ROOT app`)
 });
 
 app.get("/post-3", (req, res) => {
     res.sendFile(path.join(__dirname, '/public/posts/post_3.html'));
-    console.log(`Successfully Sent ${req.originalUrl} to ROOT app`)
 });
 
 app.get("/post-4", (req, res) => {
     res.sendFile(path.join(__dirname, '/public/posts/post_4.html'));
-    console.log(`Successfully Sent ${req.originalUrl} to ROOT app`)
 });
 
 app.get("/post-5", (req, res) => {
     res.sendFile(path.join(__dirname, '/public/posts/post_5.html'));
-    console.log(`Successfully Sent ${req.originalUrl} to ROOT app`)
 });
 
 app.get("/post-6", (req, res) => {
     res.sendFile(path.join(__dirname, '/public/posts/post_6.html'));
-    console.log(`Successfully Sent ${req.originalUrl} to ROOT app`)
 });
 
 app.get("/post-7", (req, res) => {
     res.sendFile(path.join(__dirname, '/public/posts/post_7.html'));
-    console.log(`Successfully Sent ${req.originalUrl} to ROOT app`)
 });
 
 app.get("/post-8", (req, res) => {
     res.sendFile(path.join(__dirname, '/public/posts/post_8.html'));
-    console.log(`Successfully Sent ${req.originalUrl} to ROOT app`)
 });
 
 app.get("/post-9", (req, res) => {
     res.sendFile(path.join(__dirname, '/public/posts/post_9.html'));
-    console.log(`Successfully Sent ${req.originalUrl} to ROOT app`)
 });
 
 app.get("/post-10", (req, res) => {
     res.sendFile(path.join(__dirname, '/public/posts/post_10.html'));
-    console.log(`Successfully Sent ${req.originalUrl} to ROOT app`)
 });
 
 
 // OTHER FILES
 
-
+app.get('/bg', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/other-pages/contact/bg.jpg'));
+});
 app.get('/js', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/assets/js/script.js'));
-    console.log(`Successfully Sent ${req.originalUrl} to ROOT app`)
 });
 
 app.get('/css', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/assets/css/style.css'));
-    console.log(`Successfully Sent ${req.originalUrl} to ROOT app`)
 });
 
 app.get('/ico', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/assets/images/favicon.ico'));
-    console.log(`Successfully Sent ${req.originalUrl} to ROOT app`)
 })
 
 app.get('/acss', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/other-pages/about/about.css'));
-    console.log(`Successfully Sent ${req.originalUrl} to ROOT app`)
 });
 
 app.get('/ccss', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/other-pages/contact/contact.css'));
-    console.log(`Successfully Sent ${req.originalUrl} to ROOT app`)
 });
 app.get('/pcss', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/assets/css/posts-css/post.css'));
-    console.log(`Successfully Sent ${req.originalUrl} to ROOT app`)
 });
 
 
@@ -139,50 +123,40 @@ app.get('/pcss', (req, res) => {
 
 app.get('/b1', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/assets/images/blog-1.png'));
-    console.log(`Successfully Sent ${req.originalUrl} to ROOT app`)
 });
 
 app.get('/b2', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/assets/images/blog-2.png'));
-    console.log(`Successfully Sent ${req.originalUrl} to ROOT app`)
 });
 
 app.get('/b3', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/assets/images/blog-3.png'));
-    console.log(`Successfully Sent ${req.originalUrl} to ROOT app`)
 });
 
 app.get('/b4', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/assets/images/blog-4.png'));
-    console.log(`Successfully Sent ${req.originalUrl} to ROOT app`)
 });
 
 app.get('/b5', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/assets/images/blog-5.png'));
-    console.log(`Successfully Sent ${req.originalUrl} to ROOT app`)
 });
 
 app.get('/b6', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/assets/images/blog-6.png'));
-    console.log(`Successfully Sent ${req.originalUrl} to ROOT app`)
 });
 
 app.get('/b7', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/assets/images/blog-7.png'));
-    console.log(`Successfully Sent ${req.originalUrl} to ROOT app`)
 });
 
 app.get('/b8', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/assets/images/blog-8.png'));
-    console.log(`Successfully Sent ${req.originalUrl} to ROOT app`)
 });
 
 app.get('/b9', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/assets/images/blog-9.png'));
-    console.log(`Successfully Sent ${req.originalUrl} to ROOT app`)
 });
 
 app.get('/b10', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/assets/images/blog-10.png'));
-    console.log(`Successfully Sent ${req.originalUrl} to ROOT app`)
 });
