6842d2d24a852fde321b7235c8ea275c41b3a1f6
Ahmed Mohamed <jackiejeznach@gmail.com>
2022-07-31 02:17:48 +0400

    Connected MongoDB

--- server/index.ts
+++ server/index.ts
@@ -1,29 +1,48 @@
+// Imports
 import express from 'express'
 import http, { request } from 'node:http'
 import path from 'node:path'
+import mongoose from 'mongoose'
+// Variables
 const port = 3000
 const app = express()
+const Mongouri = "mongodb+srv://back_end_1:nwePqCul7PvKTyFt@bot.it8opsm.mongodb.net/?retryWrites=true&w=majority"
+
+// Listening on port 3000
 
 app.listen(port, () => {
     console.log(`Successfully launched application to http://localhost:${port} while using HTPP`)
 })
-// send the file in ../public/index.html
-// setting it to the root app.post('/products', (req,res) => {
+
+// Using express JSON format for testing purposes
 app.use(express.json());
+
+// Connecting to mongoDB
+mongoose.connect(Mongouri, {
+}).then(() => {
+    console.log('Database Connected');
+}).catch((err) => {
+    console.log('An Error Has Occured');
+    console.log(err);
+});
+// MAIN PAGES
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
-    console.log(`Successfully Sent ${req.originalUrl} to ROOT app`)
+    console.log(`Successfully set ${req.originalUrl} as the root application`)
 
 });
 app.get("/about", (req, res) => {
     res.sendFile(path.join(__dirname, '/public/other-pages/about/about.html'));
     console.log(`Successfully Sent ${req.originalUrl} to ROOT app`)
 });
-app.get("/contact", (req, res) => {
+app.get("/contact", (req, res) => { 
     res.sendFile(path.join(__dirname, '/public/other-pages/contact/contact.html'));
     console.log(`Successfully Sent ${req.originalUrl} to ROOT app`)
 });
+
+
+
 // POSTS
 
 
@@ -106,7 +125,10 @@ app.get('/ccss', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/other-pages/contact/contact.css'));
     console.log(`Successfully Sent ${req.originalUrl} to ROOT app`)
 });
-
+app.get('/pcss', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/assets/css/posts-css/post.css'));
+    console.log(`Successfully Sent ${req.originalUrl} to ROOT app`)
+});
 
 
 
@@ -162,9 +184,5 @@ app.get('/b9', (req, res) => {
 
 app.get('/b10', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/assets/images/blog-10.png'));
-    console.log(`Successfully Sent ${req.originalUrl} to ROOT app`)
-});
-app.get('/pcss', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/assets/css/posts-css/post.css'));
     console.log(`Successfully Sent ${req.originalUrl} to ROOT app`)
 });
