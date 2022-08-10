5d52e022a6ecd0874862307b4c911a060ee94104
jckop <jackiejeznach@gmail.com>
2022-08-11 01:18:58 +0400

    Script Change

--- src/server/data/Public/assets/js/script.js
+++ src/server/data/Public/assets/js/script.js
@@ -3,7 +3,14 @@ const nav = document.querySelector('.mobile-nav');
 const navMenuBtn = document.querySelector('.nav-menu-btn');
 const navCloseBtn = document.querySelector('.nav-close-btn');
 const input = document.getElementById('email')
+const form = document.querySelector('form');
 
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    localStorage.setItem('email', input.value);
+    const email = localStorage.getItem('email');
+})
 
 // navToggle function
 const navToggleFunc = function () {
